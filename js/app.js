86f2610ccfee440ab92879c2b358de100230721b
Mike M <mikemangan00@gmail.com>
2018-03-05 16:54:54 -0500

    update to support two cats

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
-const kittyImage = document.querySelector('.kitty-image');
-const kittyCounter = document.querySelector('.kitty-counter');
-let numOfClicks = 0;
+//first kiity info
+const firstKittyImage = document.querySelector('.first-kitty-image');
+const firstKittyCounter = document.querySelector('.first-kitty-counter');
+const firstKittyName = document.querySelector('.first-kitty-name');
+let firstKittyNumOfClicks = 0;
+//second kitty info
+const secondKittyImage = document.querySelector('.second-kitty-image');
+const secondKittyCounter = document.querySelector('.second-kitty-counter');
+const secondKittyName = document.querySelector('.second-kitty-name');
+let secondKittyNumOfClicks = 0;
 
-// add eventlistener for click on the kittyImage
-kittyImage.addEventListener('click', function(){
+
+
+//update names of both kitties
+firstKittyName.textContent = 'George';
+secondKittyName.textContent = 'Alfred';
+
+// add eventlistener for click on the first kitty Image
+firstKittyImage.addEventListener('click', function(){
   // update the numOfClicks
-  numOfClicks++;
+  firstKittyNumOfClicks++;
+  // update kittyCounter
+  firstKittyCounter.textContent = firstKittyNumOfClicks;
+});
+
+// add eventlistener for click on the second kitty Image
+secondKittyImage.addEventListener('click', function(){
+  // update the numOfClicks
+  secondKittyNumOfClicks++;
   // TODO: update kittyCounter
-  kittyCounter.textContent = numOfClicks;
+  secondKittyCounter.textContent = secondKittyNumOfClicks;
 });
