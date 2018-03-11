a0686cc6dd6b66bb61440723994c70f1c5202575
Mike M <mikemangan00@gmail.com>
2018-03-10 21:53:09 -0500

    feature: add admin option to update info

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 
 const model = {
   currentCat : null,
+
   cats : [
     {
       name : 'Joe',
@@ -35,14 +36,10 @@ const model = {
 //View
 const view = {
   init : function() {
+
+    this.clearAll();
     this.renderList();
     this.renderMain();
-
-    //add clickListener for main
-    document.querySelector('img').addEventListener('click', function(){
-
-      octopus.incrementCount();
-    });
   },
 
   renderList : function() {
@@ -83,24 +80,99 @@ const view = {
     //pic
     const imageElement = document.querySelector('img');
     imageElement.setAttribute('src', cat['pic']);
+  },
+
+  clearAll : function(){
+    //clear list
+    document.querySelector('ul').innerHTML = '';
+    //clear main
   }
 };
 
+const viewAdmin = {
+  init: function() {
+    const adminBtn = document.querySelector('.admin');
+    adminBtn.addEventListener('click', function(){
+      viewAdmin.render();
+      viewAdmin.toggleVis();
+    });
+
+    const cancelBtn = document.querySelector('.cancel');
+    cancelBtn.addEventListener('click', function(){
+      viewAdmin.toggleVis();
+    });
+
+    const saveBtn = document.querySelector('.save');
+    saveBtn.addEventListener('click', function(){
+      viewAdmin.toggleVis();
+
+      const name = document.querySelector('.edit-name').value;
+      const pic = document.querySelector('.edit-pic').value;
+      const count = parseInt(document.querySelector('.edit-count').value);
+      octopus.updateCurrent(name,pic,count);
+    });
+
+  },
+
+  render : function() {
+    //get current cat
+    let cat = octopus.getCurrent();
+
+    //name
+    const editName = document.querySelector('.edit-name');
+    editName.value = cat.name;
+
+    //pic
+    const editPic = document.querySelector('.edit-pic');
+    editPic.value = cat.pic;
+
+    //count
+    const editCount = document.querySelector('.edit-count');
+    editCount.value = cat.count;
+  },
+
+  toggleVis : function() {
+    const modal = document.querySelector('.modal');
+
+    modal.classList.contains('hide') ?
+      modal.classList.remove('hide') :
+      modal.classList.add('hide');
+  }
+};
+
+//control
 const octopus = {
   init : function() {
     model.currentCat = model.cats[0];
     view.init();
+    viewAdmin.init();
 
+    //add clickListener for main
+    document.querySelector('img').addEventListener('click', function(){
+      octopus.incrementCount();
+    });
   },
+
   getList : function() {
     return model.cats;
   },
+
   getCurrent : function() {
     return model.currentCat;
   },
+
   setCurrent : function(cat) {
     model.currentCat = cat;
   },
+
+  updateCurrent : function(name,pic,count){
+    const cat = octopus.getCurrent();
+    cat.name = name;
+    cat.pic = pic;
+    cat.count = count;
+    view.init();
+  },
+
   incrementCount : function() {
     model.currentCat.count++;
     view.renderMain();
